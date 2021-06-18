33f42e82688593149d93e0a365f9ec6c051e3735
John Alban <John.Alban41@gmail.com>
2021-06-18 11:16:04 -0400

    fixes joke command

--- commands/joke.js
+++ commands/joke.js
@@ -1,16 +1,22 @@
-const JokeAPI = require('sv443-joke-api');
-const link = 'https://v2.jokeapi.dev/joke/Any'
+/*
+this command is used to send jokes, currently it can only send single joketype but eventually
+will edit to be able to send setup, punchline, category amongst other features.
+
+*/
+
+
+const fetch   = require("node-fetch")
+const link = 'https://v2.jokeapi.dev/joke/Any?type=single'
 
 
 exports.run = async (bot,message,args) => {
-    JokeAPI.getJokes(link)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-    })
+ const response  = await fetch(link).then(m => m.json()) //takes joke data from site converts to json
+ console.log(response);
 
-    message.channel.send(data)
+ message.channel.send(response.joke) // sends joke
 }
+
+
 
 
 exports.help = {
