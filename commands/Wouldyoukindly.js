7000e1807d258257941e13ebd529967caeb923c7
JohnA28 <John.Alban41@gmail.com>
2022-12-23 20:21:39 -0500

    new commit

--- commands/Wouldyoukindly.js
+++
@@ -1,9 +0,0 @@
-exports.run = async (bot,message,args) => {
-    message.channel.send("A man chooses, a slave obeys")
-}
-
-
-
-exports.help = {
-    name: "wouldyoukindly"
-}
--- index.js
+++ index.js
@@ -1,48 +1,30 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 
-const Discord = require('discord.js')
-const bot     = new Discord.Client({ws: {intents: Discord.Intents.ALL}});
-const fs      = require("fs")
-require("dotenv").config()
-bot.commands  = new Discord.Collection();
+/*
+idk what this does but it's required  
+*/
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ], 
+    partials: [
+        Partials.User,
+        Partials.Channel,
+        Partials.Message,
+        Partials.GuildMember,
+        Partials.GuildMessages,
+    ] 
+});
 
-bot.on('ready', () => {
-    console.log('Bot online')
+require('dotenv').config() //needed for env file
 
+client.events = new Collection();
 
-    //reads command directory
-    fs.readdir('./commands', (err, files) => {
-    //checks filereader for error
-        if(err) return console.log(console.err);
-        //splits commands so it will just read command instead of command.js
-        let jsfile = files.filter(f => f.split(".").pop() == 'js')
+client.on('ready', () => {
+    console.log('Bot online');
+});
 
-        if(jsfile.length == 0) {return console.log("No commands could be found")}
-
-        jsfile.forEach(f => {
-            let props = require(`./commands/${f}`);
-            bot.commands.set(props.help.name, props)
-        })
-    })
-})
-
-
-bot.on('message', (message) => {
-    if(message.author.bot) return;
-    if(message.channel.type !== 'text') return;
-// prefix to use bot
-    let prefix = '*';
-// seperates messaages by spaces
-    let MessageArray = message.content.split(' ');
-    let cmd          = MessageArray[0].slice(prefix.length)
-    let args         = MessageArray.slice(1)
-
-    if(!message.content.startsWith(prefix)) return;
-
-    let commandfile = bot.commands.get(cmd);
-    if(commandfile)   {commandfile.run(bot,message,args)}
-
-
-})
-
-
-bot.login(process.env.DISCORD_TOKEN)
+client.login(process.env.BOT_TOKEN); //connects the bot using token
