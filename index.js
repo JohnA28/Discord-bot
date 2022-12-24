5e894813b8c6414bf654af6c80cb838a19e9a36b
JohnA28 <John.Alban41@gmail.com>
2022-12-24 04:06:41 -0500

    added event handler

--- index.js
+++ index.js
@@ -35,43 +35,20 @@ for (const file of commandFiles) {
 	}
 }
 
-/*
-Every slash command is an interaction, 
-so to respond to a command, we need to create a listener for the Client#event:interactionCreate event 
-that will execute code when our bot receives an interaction.
 
-Not every interaction is a slash command (e.g. MessageComponent interactions). 
-To make sure we only handle slash commands we check with the ischatinputcommand() to make sure
-the messgae is a command and exit the handler if another type is encountered.
+//same idea as above but for events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-afterwards we return an error to the console if the inputted command doesn't exist
-otherwise the command tries to execute and if an error is encountered the error is 
-logged in the console and a message stating there was an error is sent by the bot
-*/
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
-
-
-
-client.once(Events.ClientReady, c => {
-	console.log(`Bot is online!`);
-});
+}
 
 // login for the bot using token
 client.login(process.env.TOKEN); 
